d1a7c3942c18cc11482d4a5f94436265461c4617
mehtab2899 <mehtabmultani51@gmail.com>
2021-08-24 22:58:16 +0530

    feat:version 1.0.0

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const countdownForAll = (options) => {
-  let endingDate = new Date(options.endingDate);
-  let startingDate = new Date(options.startingDate);
+const countdownForAll = (endingDate, startingDate) => {
+  let endTime = new Date(endingDate);
+  let startTime = new Date(startingDate);
 
-  const totalSeconds = (endingDate - startingDate) / 1000;
+  const totalSeconds = (endTime - startTime) / 1000;
 
   const days = Math.floor(totalSeconds / 3600 / 24);
   const hours = Math.floor(totalSeconds / 3600) % 24;
   const minutes = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
+
+  console.log(
+    `${days} days, ${hours} hour, ${minutes} minutes, ${seconds} seconds`
+  );
 };
 
 setInterval(() => {
